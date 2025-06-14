0a3ce39c78058fc8e44c00e221dfdaa738b4c323
Nyquisttt <nyquist.mailbox@gmail.com>
2025-06-14 16:43:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Assets for the Turtle Train D&D campaign.
---
+++ scripts/turtletrain.js
@@ -0,0 +1,12 @@
+console.log("[TURTLE TRAIN]: javascript loaded");
+
+const turtleTrainModPath = 'modules/nyquistt-turtle-train/';
+
+Hooks.on("init", function() {
+	console.log("[TURTLE TRAIN]: init step.");
+});
+
+Hooks.on("ready", function() {
+  console.log("[TURTLE TRAIN]: ready phase");
+});
+
